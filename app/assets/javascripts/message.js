d1744a630c4c672cd2dcfa875dd538353bd2c5bf
matsui-37 <matsuigyouhiroshi@matsuigyouhiroshinoMacBook-puro.local>
2018-10-20 11:57:27 +0900

    Correction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,13 +17,11 @@ $(function(){
 //create.json.jbuilderを通し、jbuider内で定義した変数を用いている。
 
     $('#new_message').on('submit', function(e){
-      $('.right-center').animate({scrollTop: 10000}, 10000, 'swing');
-      //animateメソッドを用いて自動スクロールを行なっている。
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
         // このactionはコメントのSENTボタンの検証から<form>内のactionを用いている。
-   
+
     $.ajax({
       url: url,
       type: 'POST',
@@ -35,26 +33,20 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.right-center').append(html);
-      $('.right-bottom__text').val('');
+      $('.right-center').animate({scrollTop: $('.right-center')[0].scrollHeight},'fast');
       $('.hidden').val('');
-      $("#disable").prop('disabled', false);
     })
     .fail(function(){
       alert('error');
+    })
+    .always(function(){
       $("#disable").prop('disabled', false);
     })
   })
 });
 
-
-
-
-
-
-
-
-
-
-
-
 // function(data)となっているが、非同期通信に成功した時の即時関数の第一引数には、サーバから返されたデータが入ってる。この場合のサーバから返ってくるデータというのは、jbuilderで作成したcreate.json.jbuilderのデータ。
+
+//animateメソッドを用いて自動スクロールを行なっている。
+
+//failとdoneに共通の処理がある時にalwaysでまとめられる。
